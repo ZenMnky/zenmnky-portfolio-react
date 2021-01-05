9cd2a52dac29d3aafdce3908514498403e584e20
Justin Hager <developerjustinhager@gmail.com>
2021-01-04 16:29:32 -0800

    minor style tweaks

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,10 +3,10 @@ import React from 'react';
 const About = () => {
     return(
         <article id='about'>
-            <div className='container'>
+            <div className='about-container'>
                 <h2>About Me</h2>
                 <div className='group'>
-                    <div className='item-double'>
+                    <div className='item'>
                         <p>Adept and versatile, I am experienced in business and program development, organizational leadership, and management of operations, policies and standards. I have 7+ years of combined experience as a leader and coach in the not for profit & health and fitness industries.</p>
                         <p>I specialize in designing and building solutions and processes from the ground up to support an organization’s objectives, managing solution lifecycles from conceptualization thru operationalization. Involved in web application development since December 2019, I bring creative solutions, organization, and collaboration.</p>
                         <p>I am now seeking to contribute my experience and expertise to a software development project in Portland, Oregon, or remotely.</p>
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -12,7 +12,7 @@ const Intro = () => {
                             className="main-img rounded"
                         />
                     </div>
-                    <div className="item">
+                    <div className="item intro-para">
                         <p className="headline"> Hi, I'm Justin Hager.</p>
                         <p>Adept and versatile,  I am experienced in business and program development, organizational leadership, and management of operations, policies and standards</p>
                         <p> I have 7+ years of combined experience as a leader and coach in the not for profit & health and fitness industries.</p>
