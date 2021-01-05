30f1f07a17bf59adb4169a8fa43f607ce5ab5123
Justin Hager <developerjustinhager@gmail.com>
2021-01-05 10:51:11 -0800

    spelling fix

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ const store = {
         {
             img: 'https://i.ibb.co/nQrncMk/placeholder.png',
             title: 'AcroYoga Skills Development App',
-            description: 'This app does great things, for ordinary and extraordinary people, becuase it needed to exist',
+            description: 'This app does great things, for ordinary and extraordinary people, because it needed to exist',
             techStack: 'Node.js, Express, React, Sequelize, MongoDB',
             repoLink: '#projects',
             liveLink: '#projects',
@@ -11,7 +11,7 @@ const store = {
         {
             img: 'https://i.ibb.co/nQrncMk/placeholder.png',
             title: 'Personal Life Coach',
-            description: 'This app does great things, for ordinary and extraordinary people, becuase it needed to exist',
+            description: 'This app does great things, for ordinary and extraordinary people, because it needed to exist',
             techStack: 'Node.js, Express, React, Sequelize, MongoDB',
             repoLink: '#projects',
             liveLink: '#projects',
@@ -19,7 +19,7 @@ const store = {
         {
             img: 'https://i.ibb.co/nQrncMk/placeholder.png',
             title: 'Private Life Journal and Tracker',
-            description: 'This app does great things, for ordinary and extraordinary people, becuase it needed to exist',
+            description: 'This app does great things, for ordinary and extraordinary people, because it needed to exist',
             techStack: 'Node.js, Express, React, Sequelize, MongoDB',
             repoLink: '#projects',
             liveLink: '#projects',
@@ -27,7 +27,7 @@ const store = {
         {
             img: 'https://i.ibb.co/nQrncMk/placeholder.png',
             title: 'Polar HR Data to Google Calendar',
-            description: 'This app does great things, for ordinary and extraordinary people, becuase it needed to exist',
+            description: 'This app does great things, for ordinary and extraordinary people, because it needed to exist',
             techStack: 'Node.js, Express, React, Sequelize, MongoDB',
             repoLink: '#projects',
             liveLink: '#projects',
