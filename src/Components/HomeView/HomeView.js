8da8f7f636e3743e477ff278a4e3cf7b5a362b65
Justin Hager <coachjustinhager@gmail.com>
2021-02-25 11:46:30 -0800

    adds browser-router and home route. header logo set to a link. adds 404 page - needs style work

---
+++ src/Components/HomeView/HomeView.js
@@ -0,0 +1,16 @@
+
+import Intro from '../Intro/Intro';
+import About from '../About/About';
+import Projects from '../Projects/Projects';
+import Contact from '../Contact/Contact';
+
+export default function HomeView() {
+    return (
+        <>
+            <Intro />
+            <About />
+            <Projects />
+            <Contact />
+        </>
+    )
+}
---
+++ src/Components/PageNotFound/PageNotFound.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class PageNotFound extends Component {
+  render() {
+    return (
+      <div className='notfound-page'>
+        <p>Sorry! We couldn't find the page you were looking for.</p>    
+      </div>
+    );
+  }
+}
+
+export default PageNotFound;
