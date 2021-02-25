93c729a805f7ff1af51d88a909fb0260494bb35b
Justin Hager <coachjustinhager@gmail.com>
2021-02-24 16:45:34 -0800

    remove services from nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,6 @@ const Header = () => {
                 <ul>
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
-                    <li><a href="#services">Services</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
