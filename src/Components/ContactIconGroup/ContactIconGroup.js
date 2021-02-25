cee2221a0dff1ee59f50fb6960a39aa1ced27ed8
Justin Hager <coachjustinhager@gmail.com>
2021-02-24 18:18:51 -0800

    replace blog icon with dev icon

--- src/Components/ContactIconGroup/ContactIconGroup.js
+++ src/Components/ContactIconGroup/ContactIconGroup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faDev } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faBlog } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -8,14 +8,15 @@ import { faEnvelope, faBlog } from '@fortawesome/free-solid-svg-icons';
 const ContactIconGroup = () => {
     return (
         <div className='contact-icon-group'>
-            <a href='https://dev.to/zenmnky'>
-                <FontAwesomeIcon icon={faBlog} className='fa'/>
-            </a>
             
             <a href='https://www.linkedin.com/in/developerjustinhager/' target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon icon={faLinkedin} className='fa' />
             </a>
             
+            <a href='https://dev.to/zenmnky'>
+                <FontAwesomeIcon icon={faDev} className='fa'/>
+            </a>
+
             <a href='https://github.com/zenmnky' target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon icon={faGithub} className='fa'/>
             </a>
